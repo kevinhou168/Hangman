aac17c12969205c7695bc4a3bf4da57be2260a51
kevinhou168 <kevinhou168@gmail.com>
2019-01-09 23:46:20 -0600

    Revise hangman and fill_blanks variables
    Different variable types caused them to mismatch, making the win condition always false. Also resolved both issues pertaining to upper-case lettering and displaying words as a list.

--- Hangman.py
+++ Hangman.py
@@ -6,10 +6,12 @@ people = ['boy', 'cat', 'bird', 'girl', 'dog', 'Northwestern student', 'blue foo
 verbs = ['leapt over', 'attacked', 'climbed on', 'created', 'destroyed', 'flipped over', 'hugged', 'kissed']
 objects = ['mountain', 'hill', 'building', 'desk', 'satellite', 'computer', 'tree', 'lecture hall']
 
+
 def random_word():
     word_list = people + objects
     num = random.randrange(0, len(word_list))
     return word_list[num]
+
 
 def random_sentence():
     num_people = random.randrange(0, len(people))
@@ -17,17 +19,19 @@ def random_sentence():
     num_objects = random.randrange(0, len(objects))
     return "The " +people[num_people]+ " " +verbs[num_verbs]+ " the " +objects[num_objects]
 
+
 def result(win):
     if win == True:
-        print("Congratulations, you saved the hangman! But from what did you save him from...?")
+        print("Congratulations, you saved the hangman! He goes on to live another day!")
     else:
-        print("Oh no! You failed to save the hangman. There seems to be another one though, for some reason.")
+        print("You failed to save the hangman from the doom that was " + hangman)
 
     if input("Would you like to play again? (Y/N)") == 'Y':
         game()
     elif input("Would you like to play again? (Y/N)") == 'N':
         print("Thank you for playing!")
         exit()
+
 
 def game():
     difficulty = input(player_name + ', would you like a word or a sentence?\n')
@@ -50,28 +54,36 @@ def game():
     num_guesses = len(hangman)
     print('You have ' + str(num_guesses) + ' guesses to save the hangman. It is up to you, ' + player_name + '.')
 
-    for i in range(0, num_guesses):
+    while num_guesses > 0:
         guess = input('Enter an upper-case letter: \n')
+        check = False
         for j in range(0, len(hangman)):
-            if hangman[j] == guess:
+            if hangman[j].lower() == guess.lower():
                 fill_blanks[j] = hangman[j]
+                check = True
 
-        num_guesses -= 1
-        guessed_letters.append(guess)
-        print(fill_blanks)
-        print("So far, you have guessed: \n" + str(guessed_letters))
+        if check == False:
+            num_guesses -= 1
+
+
 
         if num_guesses == 1:
             print("You have 1 remaining guess.")
         elif num_guesses == 0:
-            if hangman == fill_blanks:
+            if hangman == (''.join(fill_blanks)):
                 result(True)
             else:
                 result(False)
         else:
-            print("You have " + num_guesses + " remaining guesses.")
+            print("You have " + str(num_guesses) + " remaining guesses.")
 
-        if hangman == fill_blanks:
+        if hangman == (''.join(fill_blanks)):
+            print(''.join(fill_blanks))
             result(True)
+        else:
+            guessed_letters.append(guess)
+            print(''.join(fill_blanks))
+            print("So far, you have guessed: \n" + (', '.join(guessed_letters)))
+
 
 game()
